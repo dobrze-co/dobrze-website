c18dda15792d1672f59a0bb06fce5a6c813b46cf
wojciechpanek <wojciech.panek@fourthwall.com>
2021-07-30 20:45:26 +0200

    Remove about us paragraph

--- src/data/aboutUs.js
+++ src/data/aboutUs.js
@@ -14,7 +14,6 @@ export default [
     paragraphs: [
       "W <strong>dobrze.</strong> zajmuję się analizą i projektowaniem wizerunku Twojej marki. Sprawdzam czy Twoja obecna strona i treści docierają do odpowiednich osób. Dbam o to, żeby każde działanie było odpowiednio zmierzone.",
       "Pomagam Ci znaleźć odpowiednią formę graficzną, która będzie wizytówką Twojego biznesu online i offline. Tworzę obraz, który dopełnia to co chcesz przekazać.  Od 6 lat pracuję w branży IT, dbam o rozwój biznesów internetowych, dotarcie do jak największej ilości klientów i budowanie strategii marketingowej.",
-      "Lubię rzeczy przyjemne dla oka. Jestem założycielką firmy dekoratorskiej, właścicielką 2 kotów i 6 rowerów. <strong>dobrze.</strong> to dla mnie przede wszystkim realizacja pasji.",
     ],
   },
   {
